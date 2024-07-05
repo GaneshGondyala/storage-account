405f761d75268716b4de413d321f97eb3650e997
GaneshGondyala <139107137+GaneshGondyala@users.noreply.github.com>
2024-07-05 13:14:03 +0530

    Update variables.tf

--- Terraform-manifest/variables.tf
+++ Terraform-manifest/variables.tf
@@ -1,13 +1,13 @@
 variable "resource_group_name" {
   description = "The name of the resource group in which to create the storage account"
   type        = string
-  default     = "ganeshgondylala1729_group"
+  default     = "ganeshgondylala1729_group123"
 }
 
 variable "location" {
   description = "The Azure region to deploy to"
   type        = string
-  default     = "East us"
+  default     = "(US) East US"
 }
 
 variable "storage_account_name" {
